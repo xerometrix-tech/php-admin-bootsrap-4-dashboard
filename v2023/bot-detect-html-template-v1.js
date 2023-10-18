bb5887a40e48a12fde86cf3cb63b4af18ef5c74c
Alexander Wayne Omorokunwa <xeroxism@gmail.com>
2023-10-18 17:21:52 +0100

    Create bot-detect-html-template-v1.js
    detecting bots v1

---
+++ v2023/bot-detect-html-template-v1.js
@@ -0,0 +1,26 @@
+//Add the following code to the template’s main file:
+//This script will check the user agent and the number of requests made in the last time interval to detect bot traffic.
+//If the user is detected to be a bot, you can take action, such as blocking the user or displaying a warning message.
+//========================================//
+
+// Get the request count and time interval
+var requestCount = 0;
+var timeInterval = 10; // seconds
+
+
+// Start a timer to track the number of requests in the last time interval
+setInterval(function() {
+  requestCount = 0;
+}, timeInterval * 1000);
+
+
+// Add an event listener to count the number of requests
+window.addEventListener("request", function() {
+  requestCount++;
+});
+
+
+// Check if the user is a bot
+if (detectBotTraffic()) {
+  // Take action, such as blocking the user or displaying a warning message
+}
